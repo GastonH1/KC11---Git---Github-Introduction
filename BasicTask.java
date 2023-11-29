cd4f2d9f63b5cb04340ce1b0f8cb1bfca87dfe38
GastonH1 <ghonomou7492@students.mc3.edu>
2023-11-28 23:22:25 -0500

    Create BasicTask.java
    Added a java program that performs basic task like print a text and use a loop.

---
+++ BasicTask.java
@@ -0,0 +1,13 @@
+public class BasicTask{
+   public static void main(String[] args){
+      
+      // Print a simple text on the screen
+      System.out.println("Hello World");
+      
+      // Printnumbers from 1 to 100
+      for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }    
+      
+   }
+}
